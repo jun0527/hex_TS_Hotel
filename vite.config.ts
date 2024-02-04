2e4dd457d27cdd46eb8cdfbb9898f78ea4ae19de
yijun <sandy3068@yahoo.com.tw>
2024-02-04 23:44:00 +0800

    feat: add deploy.sh

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/example-vite-react/" : "/",
   plugins: [
     react(),
     createSvgIconsPlugin({
